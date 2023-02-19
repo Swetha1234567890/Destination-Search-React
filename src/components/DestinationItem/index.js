b017b0c68bf196a5e904ac5389a4aefdb7846c21
mantri swetha <mantri swetha98@gmail.com>
2023-02-19 15:59:48 +0000

    Destination Search

---
+++ src/components/DestinationItem/index.js
@@ -0,0 +1,16 @@
+// Write your code here
+import './index.css'
+
+const DestinationItem = props => {
+  const {destinationDetails} = props
+  const {name, imgUrl} = destinationDetails
+
+  return (
+    <li className="img-container">
+      <img src={imgUrl} className="image" alt={name} />
+      <p className="name">{name}</p>
+    </li>
+  )
+}
+
+export default DestinationItem
---
+++ src/components/DestinationSearch/index.js
@@ -0,0 +1,53 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+import DestinationItem from '../DestinationItem'
+
+class DestinationSearch extends Component {
+  state = {
+    searchInput: '',
+  }
+
+  onChangeSearch = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  render() {
+    const {searchInput} = this.state
+    const {destinationsList} = this.props
+    const searchResults = destinationsList.filter(each =>
+      each.name.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+    return (
+      <div className="app-container">
+        <div className="background">
+          <h1 className="heading">Destination Search</h1>
+          <div className="search-container">
+            <input
+              type="search"
+              placeholder="Search"
+              onChange={this.onChangeSearch}
+              value={searchInput}
+              className="search-input"
+            />
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
+              className="search-icon"
+              alt="search icon"
+            />
+          </div>
+          <ul className="destination-list">
+            {searchResults.map(eachItem => (
+              <DestinationItem
+                key={eachItem.id}
+                destinationDetails={eachItem}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default DestinationSearch
